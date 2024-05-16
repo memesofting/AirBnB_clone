98834c3c5061b310e36e6499a408897fee983b6c
memesofting <bibitayoadekunle00@gmail.com>
2024-05-16 15:26:13 +0100

    base model dictionary representation done

--- models/base_model.py
+++ models/base_model.py
@@ -15,27 +15,27 @@ class BaseModel:
         if id is None:
             self.id = str(uuid.uuid4())
         else:
-            self.id = id
+            self.id = str(id)
         if created_at is None:
-            self.created_at = datetime.now()
+            self.created_at = str(datetime.now())
         else:
             self.created_at = created_at
         if updated_at is None:
-            self.updated_at = datetime.now()
+            self.updated_at = str(datetime.now())
         else:
             self.updated_at = updated_at
 
     def save(self):
         """Updates the instance attribute (updated_at)
         with current datetime"""
-        
-        pass
+        self.updated_at = str(datetime.now())
+        """return self.updated_at"""
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of the __dict__
         of the instance"""
-        
-        pass
+        self.__dict__['__class__'] = self.__class__.__name__
+        return self.__dict__
 
     def __str__(self):
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
