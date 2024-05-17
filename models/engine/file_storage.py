40df324220c0f438ce08ab3e6749219dfb6ed241
memesofting <bibitayoadekunle00@gmail.com>
2024-05-17 21:35:07 +0000

    working o deserialisation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,25 +2,33 @@
 """This is a file storage module"""
 
 
+import json
+from models.base_model import BaseModel
+
+
 class FileStorage:
     """FileStorage class serializes instances to JSON file
     and deserializes JSON files to instances"""
-    
+
     __file_path = "storage.json"
     __objects = {}
-    
+
     def all(self):
         """Returns the dictionary __objects"""
-        pass
-    
+        return self.__objects
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        pass
-    
+        objKey = f'{obj.__class__.__name__}.{obj.id}'
+        self.__obk[key] = obj
+
     def save(self):
         """serializes __objects to JSON file (path: __file_path)"""
-        pass
-    
+        for key, obj in self.__objects.item():
+            obj_dict = {key: obj.to_dict()}
+        with open(self.__filepath, 'w') as file:
+            json.dump(self.obj_dict, file)
+
     def reload(self):
         """deserializes the JSON file to __objects
         Only if the JSON file (__file_path) exists
